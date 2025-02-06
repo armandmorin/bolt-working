6af6375dec4e57700fa6d68c72016b8a840e0c80
armandmorin <armandmorin@gmail.com>
2025-02-05 22:29:08 -0500

    Initial commit

--- src/components/WidgetCodeSnippet.jsx
+++ src/components/WidgetCodeSnippet.jsx
@@ -10,8 +10,6 @@ const WidgetCodeSnippet = ({ clientKey }) => {
   (function() {
     const script = document.createElement('script');
     script.src = "${window.location.origin}/widget/accessibility-widget.js";
-    script.setAttribute('data-supabase-url', 'https://hkurtvvrgwlgpbyfbmup.supabase.co');
-    script.setAttribute('data-supabase-key', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhrdXJ0dnZyZ3dsZ3BieWZibXVwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc1NTkyOTksImV4cCI6MjA1MzEzNTI5OX0.T8kS-k8XIcTzAHiX7NWQQtJ6Nkf7OFOsUYsIFAiL37o');
     script.setAttribute('data-client-key', '${clientKey}');
     document.body.appendChild(script);
   })();
